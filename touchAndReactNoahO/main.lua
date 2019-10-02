dfce7ff193df53bef2a8b9b896d8ba1d8d8711b5
Noah Ouellette <noah.ouellette@ocsbstudent.ca>
2019-10-02 14:47:15 -0400

    addededed

--- touchAndReactNoahO/main.lua
+++ touchAndReactNoahO/main.lua
@@ -28,15 +28,32 @@ textObject.y = display.contentHeight/3
 textObject:setTextColor (1, 1, 0)
 textObject.isVisible = false
 
+--create the checkmark
+local check = display.newImageRect("Images/checkmark.png", 150, 150)
+check.x = display.contentWidth/2
+check.y = display.contentHeight/1.5
+check.isVisible = false
+
+local x = display.newImageRect("Images/red_x.png", 150, 150)
+x.x = display.contentWidth/2
+x.y = display.contentHeight/1.5
+x.isVisible = false
 --Function: BlueButtonListener
 --Input: touch Listener
 --Output: none
 --Description: when blue button is clicked, it will make the text appear with the red button, and make the blue button disappear
+
+--create the sound
+local correctSound = audio.loadSound( "Sounds/Correct Answer Sound Effect.mp3" )
+local wrongSound = audio.loadSound( "Sounds/Wrong Buzzer Sound Effect.mp3")
 local function BlueButtonListener(touch)
 	if (touch.phase == "began") then
 		blueButton.isVisible = false
 		redButton.isVisible = true
 		textObject.isVisible = true
+		check.isVisible = true
+		x.isVisible = false
+		audio.play( correctSound )
 	end
 
 	if(touch.phase == "ended") then
@@ -46,27 +63,25 @@ local function BlueButtonListener(touch)
 	end
 end
 
---listeners
-blueButton:addEventListener("touch", BlueButtonListener)
-
---Function: RedButtonListener
---Input: touch Listener
---Output: none
---Description: when Red button is clicked, it will make the text disappear with the blue button, and make the red button disappear
 local function RedButtonListener(touch)
 	if (touch.phase == "began") then
-		redButton.isVisible = false
-		blueButton.isVisible = true
-		textObject.isVisible = false
-	end
-
-	if(touch.phase == "ended") then
 		redButton.isVisible = true
 		blueButton.isVisible = false
 		textObject.isVisible = true
+		
+	end
+
+	if(touch.phase == "ended") then
+		redButton.isVisible = false
+		blueButton.isVisible = true
+		textObject.isVisible = false
+		check.isVisible = false
+		x.isVisible = true
+		audio.play( wrongSound )
+	
 	end
 end
-
 --listeners
+blueButton:addEventListener("touch", BlueButtonListener)
 redButton:addEventListener("touch", RedButtonListener)
 
