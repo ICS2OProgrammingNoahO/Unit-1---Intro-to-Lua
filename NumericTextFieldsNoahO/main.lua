aefbf48afe7b5a65ff14e1e737d3d9a1f4643b66
Noah Ouellette <noah.ouellette@ocsbstudent.ca>
2019-10-16 14:34:49 -0400

    gvj

--- NumericTextFieldsNoahO/main.lua
+++ NumericTextFieldsNoahO/main.lua
@@ -28,19 +28,59 @@ local points_ = 0
 local youLose
 local youWin
 local correctAnswer1
-local lives = 3
+local lives = 4
 local livesText
 local realAnswer
 local rightSound = audio.loadSound( "beep.mp3")
-local WrongSound = audio.loadSound( "wrong.mp3")
+local wrongSound = audio.loadSound( "wrong.mp3")
 local channel1
 local channel2
+local totalSeconds = 10
+local secondsLeft = 10
+local countDownTimer
+local heart1
+local heart2
+local heart3
+local heart4
+local clockText
+local clockObject
+
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+local function 	UpdateTime()
+	-- decrement the number of secondsLeft
+	secondsLeft = secondsLeft - 1
+
+	-- display the number of seconds left in the clock object
+	clockText.text = secondsLeft .. ""
+
+	if (secondsLeft == 0) then
+		--reset the number of seconds left
+		secondsLeft = totalSeconds
+		lives = lives - 1
+
+		--*** IF THERE ARE NO LIVES LEFT, PLAY A LOSE SOUND, SHOW A YOU LOSE IMAGE
+		-- AND CANCEL THE TIMER REMOVE THE THIRD HEART BY MAKING IT INVISIBLE\
+				if (lives == 2) then
+					heart2.isVisible = false
+				elseif (lives == 1) then
+					heart1.isVisible = false
+				end
+	end
+end
+
+-- function that calls the timer
+local function StartTimer()
+	--create a countDown timer that loops infinitly
+	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+end
+
 local function AskQuestion()
+	
+	StartTimer()
 	--generate a random number between 1 and 4 and declare it's variable
 	randomOperator = math.random(1, 4)
 
@@ -49,7 +89,7 @@ local function AskQuestion()
 	randomNumber2 = math.random(0, 10)
 	randomNumber3 = math.random(2, 12)
 	randomNumber4 = math.random(2, 12)
-
+ 
 	if (randomOperator == 1) then
 
 	correctAnswer = randomNumber1 + randomNumber2
@@ -86,14 +126,11 @@ local function HideCorrect()
 
 	correctObject.isVisible = false
 	incorrectObject.isVisible = false
+	realAnswer.isVisible = false
 	AskQuestion()
 
 end 
-local function HideRealAnswer()
 
-	realAnswer.isVisible = false
-	AskQuestion()
-end
 
 local function NumericFeildListener( event )
 	-- User begins editing "numericfield"
@@ -110,8 +147,10 @@ local function NumericFeildListener( event )
 		--if the users answer and the correct answer are the same:
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
-			timer.performWithDelay(500, HideCorrect)
+			incorrectObject.isVisible = false
+			timer.performWithDelay(700, HideCorrect)
 			channel1 = audio.play(rightSound)
+
 		--if the users answer and the correct answer are different:
 		--give points
 		points = points +1
@@ -133,12 +172,11 @@ local function NumericFeildListener( event )
 		else incorrectObject.isVisible = true
 			realAnswer = display.newText("The real answer is " .. correctAnswer, display.contentWidth/2, display.contentHeight/9, nil, 50)
 			realAnswer.isVisible = true
+			timer.performWithDelay(700, HideCorrect)
 			channel2 = audio.play(wrongSound)
-			timer.performWithDelay(1000, HideRealAnswer)
-
-
 			points_ = points_ + 1
 			 lives = lives - 1
+
 			-- update it in display object
 				livesText.text = "Lives = " .. lives
 				
@@ -163,6 +201,25 @@ end
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- OBJECT CREATION
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--create the hearts
+heart1 = display.newImageRect("heart.png", 50, 50)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight - 200
+
+heart2 = display.newImageRect("heart.png", 50, 50)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight - 200
+
+heart3 = display.newImageRect("heart.png", 50, 50)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight - 200
+
+heart4 = display.newImageRect("heart.png", 50, 50)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight - 200
+
+
 
 --displays a question and sets the color
 --questionObject = display.newText( "", display.contentWidth/2, display.contentHeight/2, nil, 50 )
@@ -201,6 +258,8 @@ livesText = display.newText("Lives =" .. lives, display.contentWidth/2, display.
 pointsText = display.newText("Points =" .. points, display.contentWidth/2, display.contentHeight/4, nil, 50)
 -- add the event listener for the numeric feild
 numericfield:addEventListener( "userInput", NumericFeildListener)
+
+clockText = display.newText( secondsLeft, 100, 100, nil, 50)
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- FUNCTION CALLS
