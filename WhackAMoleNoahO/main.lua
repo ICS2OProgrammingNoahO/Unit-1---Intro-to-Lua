921de7da7e793446b535a5452c2198faa3afeb74
Noah Ouellette <55556015+s276968@users.noreply.github.com>
2019-10-20 20:09:20 -0400

    Finished WHACKAELON

--- WhackAMoleNoahO/main.lua
+++ WhackAMoleNoahO/main.lua
@@ -7,24 +7,19 @@
 local background = display.newImageRect("Images/X.gif", 1000, 1000)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
-
 --hide status bar
 display.setStatusBar(display.HiddenStatusBar)
+-------------------------------------------------------------------------------------------------------------
+--Variables
+-------------------------------------------------------------------------------------------------------------
 
---creating WhackAnElon
-local elon = display.newImage("Images/elon.png", 0, 0 )
-
-	--setting position
-	elon.x = display.contentCenterX
-	elon.y = display.contentCenterY
---make it a third of its size
-	elon.xScale = 1/3
-	elon.yScale = 1/3
---make it invisible
-	elon.isVisible = false
--- create score and score text
+local elon
 local score = 0
-local scoreText = display.newText("SCORE = ".."0", 700, 100, nil, 70)
+local scoreText
+local channel
+local channel2
+local music = audio.loadStream("Sounds/music.mp3")
+local click = audio.loadSound("Sounds/click.mp3")			
 
 -------------------------------------------------------------------------------------------------------------
 --FUNCTIONS
@@ -35,13 +30,19 @@ function PopUp( )
 	-- choosing random position on the screen between 0 and the size of the screen
 	elon.x = math.random( 0, display.contentWidth )
 	elon.y = math.random( 0, display.contentHeight )
+	
+	num1 = math.random( 1, 3)
+	num2 = math.random( 4, 8)
+	--make it a different size
 
+	elon.xScale = num1/num2
+	elon.yScale = num1/num2
 	elon.isVisible = true
 
 	timer.performWithDelay(250, Hide())
 end
 
---this function calls popup after three seconds
+--this function calls popup after one second
 function PopUpDelay( )
 	timer.performWithDelay( 2000, PopUp)
 end
@@ -59,15 +60,40 @@ end
 
 --this function increments the score only if elon is clicked. It then displays the new score.
 function Whacked( event )
+		
 	--if touch phase just started
 	if (event.phase == "began") then
 		score = score + 1
 		scoreText.text = "SCORE = ".. score
+		channel2 = audio.play(click)
+		elon.isVisible = false
+		
+		
 	end
 end
+
+local function SoundTrack()
+	channel = audio.play(music, {loop = -1})
+end
+
+-------------------------------------------------------------------------------------------------------------
+--Object Creation
+-------------------------------------------------------------------------------------------------------------
+
+elon = display.newImage("Images/elon.png", 0, 0 )
+
+	--setting position
+	elon.x = display.contentCenterX
+	elon.y = display.contentCenterY
+	--make it invisible
+	elon.isVisible = false
+
+scoreText = display.newText("SCORE = ".."0", 800, 350, nil, 30)
+		scoreText:setFillColor(255, 255, 0)
 
 -------------------------------------------------------------------------------------------------------------
 --listeners
 -------------------------------------------------------------------------------------------------------------
  elon:addEventListener( "touch", Whacked)
  GameStart()
+ SoundTrack()
